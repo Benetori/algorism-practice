fcece7689f45899a6603576fdfdb388c3fbda4d6
DongHwan-Gim <benetori@gmail.com>
2018-11-21 14:16:16 +0900

    Update Drawrect scala file
    Update better way

--- Algospot/src/4. Drawrect/Drawrect.scala
+++ Algospot/src/4. Drawrect/Drawrect.scala
@@ -2,23 +2,23 @@ object Drawrect {
   def main(args: Array[String]): Unit = {
 	var cases = Integer.parseInt(readLine())
 	while (cases > 0) {
-		var result: List[Int] = null
+		var result = List[Int]()
 		val a: List[Int] = readLine().split(" ").map(_.toInt).toList
 		val b: List[Int] = readLine().split(" ").map(_.toInt).toList
 		val c: List[Int] = readLine().split(" ").map(_.toInt).toList
 		if ( a(0) == b(0) ) {
-			result =  List(c(0))
+			result :+ c(0)
 		} else if ( a(0) == c(0) ) {
-			result =  List(b(0))
+			result :+ b(0)
 		} else {
-			result =  List(a(0))
+			result :+ a(0)
 		}
 		if ( a(1) == b(1) ) {
-			result =  result :+ c(1)
+			result :+ c(1)
 		} else if ( a(1) == c(1) ) {
-			result =  result :+ b(1)
+			result :+ b(1)
 		} else {
-			result =  result :+ a(1)
+			result :+ a(1)
 		}
 	println(result.mkString(" "))
 	cases -= 1
